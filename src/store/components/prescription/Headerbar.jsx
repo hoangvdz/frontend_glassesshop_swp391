50f7db5ce59d3f1cfee3ae17e933e760e9637663
Ducne123-456 <ducnvse182993@fpt.edu.vn>
2026-04-09 19:35:43 +0700

    Clean up database and UI: Removed PD and Prism references from both frontend and backend.

--- src/store/components/prescription/Headerbar.jsx
+++ src/store/components/prescription/Headerbar.jsx
@@ -4,7 +4,7 @@ import { ArrowLeft, X, HelpCircle } from "lucide-react";
 export default function HeaderBar() {
   const navigate = useNavigate();
   return (
-    <div className="flex items-center justify-between px-6 py-4 border-b bg-white">
+    <div className="sticky top-[68px] z-[40] h-16 flex items-center justify-between px-8 bg-white border-b border-gray-100 shadow-sm transition-all duration-300">
 
       <button className="flex items-center gap-2 text-sm  hover:bg-gray-100 rounded-full transition" onClick={() => navigate("/")}>
         <ArrowLeft size={18}/>
